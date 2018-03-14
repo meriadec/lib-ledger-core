76d7f3e75d10b973fa7227ef37f0f76ab5a3c64b
KhalilBellakrid <khalil.bellakrid@ledger.fr>
2018-03-14 21:16:35 +0100

    node and yarn install

--- .circleci/setup_debian.sh
+++ .circleci/setup_debian.sh
@@ -1,4 +1,15 @@
 #!/usr/bin/env bash
 
+echo "========> Install basic config"
 apt-get update
 apt-get install -y g++ make cmake wget yarn nodejs git build-essential libx11-xcb-dev qt5-default libqt5websockets5 libqt5websockets5-dev
+
+
+echo "========> Install node"
+curl -sL https://deb.nodesource.com/setup_9.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+echo "========> Install yarn"
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
+apt-get update && apt-get install -y yarn
