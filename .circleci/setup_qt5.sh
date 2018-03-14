9d26e0331fd2d27920eec393b1fa31182514a8cf
KhalilBellakrid <khalil.bellakrid@ledger.fr>
2018-03-14 13:42:03 +0100

    get qt5 from github

--- .circleci/setup_qt5.sh
+++ .circleci/setup_qt5.sh
@@ -1,15 +1,39 @@
 #!/usr/bin/env bash
 
-echo "Download Qt5"
-wget http://download.qt.io/official_releases/qt/5.7/5.7.0/qt-opensource-linux-x64-5.7.0.run
-
-echo "Install g++"
-apt-get install build-essential
+#echo "Download Qt5"
+#wget http://download.qt.io/official_releases/qt/5.7/5.7.0/qt-opensource-linux-x64-5.7.0.run
+#
+#echo "Install g++"
+#apt-get install build-essential
+#
+#echo "Install Qt5 dependencies"
+#apt-get install -y libfontconfig1 libdbus-1-dev mesa-common-dev libglu1-mesa-dev
+#
+#echo "Install Qt5"
+#chmod +x qt-opensource-linux-x64-5.7.0.run
+#./qt-opensource-linux-x64-5.7.0.run
 
 echo "Install Qt5 dependencies"
-apt-get install -y libfontconfig1 libdbus-1-dev mesa-common-dev libglu1-mesa-dev
+apt-get install -y libxcb*
+apt-get install -y libx11-xcb-dev
 
-echo "Install Qt5"
-chmod +x qt-opensource-linux-x64-5.7.0.run
-./qt-opensource-linux-x64-5.7.0.run
+echo "Clone Qt5 repository"
+git clone git://code.qt.io/qt/qt5.git qt5
+
+echo "Init Qt5 repository"
+cd qt5
+./init-repository
+
+echo "Add Qt5 au Path"
+export PATH=$PWD/qtbase/bin:$PATH
+
+echo "Configurate Qt5"
+./configure -nokia-developer -nomake examples -nomake demos -nomake tests -no-gtkstyle
+
+echo "Build Qt5"
+make -j3
+
+cd ..
+
+
 
