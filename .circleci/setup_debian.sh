23aeef7709adf13f852baa739e6414ca71a75b16
KhalilBellakrid <khalil.bellakrid@ledger.fr>
2018-03-14 17:00:09 +0100

    add qt5-default

--- .circleci/setup_debian.sh
+++ .circleci/setup_debian.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 apt-get update
-apt-get install -y g++ make cmake wget git build-essential libx11-xcb-dev
+apt-get install -y g++ make cmake wget git build-essential libx11-xcb-dev qt5-default
