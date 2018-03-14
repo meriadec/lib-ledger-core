d77f83a08d24b29b5e0958cf3abf3e244e1b2152
KhalilBellakrid <khalil.bellakrid@ledger.fr>
2018-03-14 17:55:32 +0100

    add qt5 websockets

--- .circleci/setup_debian.sh
+++ .circleci/setup_debian.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 apt-get update
-apt-get install -y g++ make cmake wget git build-essential libx11-xcb-dev qt5-default
+apt-get install -y g++ make cmake wget git build-essential libx11-xcb-dev qt5-default libqt5websockets5 libqt5websockets5-dev
