c08628f3c364c8df5a28c6c6070b846af0de5400
KhalilBellakrid <khalil.bellakrid@ledger.fr>
2018-03-14 11:21:59 +0100

    add libfontconfig

--- .circleci/setup_qt5.sh
+++ .circleci/setup_qt5.sh
@@ -10,6 +10,9 @@ chmod +x qt-opensource-linux-x64-5.7.0.run
 echo "Install g++"
 sudo apt-get install build-essential
 
+echo "Install generic font configuration library "
+sudo apt-get install libfontconfig1
+
 echo "Install OpenGL"
 sudo apt-get install mesa-common-dev
 sudo apt-get install libglu1-mesa-dev -y
