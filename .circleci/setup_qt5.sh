d5df831fed14717a108f6c65496884e2b4940aea
KhalilBellakrid <khalil.bellakrid@ledger.fr>
2018-03-14 11:42:36 +0100

    fix qt5 install

--- .circleci/setup_qt5.sh
+++ .circleci/setup_qt5.sh
@@ -3,10 +3,6 @@
 echo "Download Qt5"
 wget http://download.qt.io/official_releases/qt/5.7/5.7.0/qt-opensource-linux-x64-5.7.0.run
 
-echo "Install Qt5"
-chmod +x qt-opensource-linux-x64-5.7.0.run
-./qt-opensource-linux-x64-5.7.0.run
-
 echo "Install g++"
 sudo apt-get install build-essential
 
@@ -16,4 +12,8 @@ sudo apt-get install libfontconfig1
 echo "Install OpenGL"
 sudo apt-get install mesa-common-dev
 sudo apt-get install libglu1-mesa-dev -y
+
+echo "Install Qt5"
+chmod +x qt-opensource-linux-x64-5.7.0.run
+./qt-opensource-linux-x64-5.7.0.run
 
