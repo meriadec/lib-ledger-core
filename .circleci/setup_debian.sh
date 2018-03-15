ee377546852ede3a7d5cec1ef7e9ed99e8d77738
KhalilBellakrid <khalil.bellakrid@ledger.fr>
2018-03-15 13:12:59 +0100

    add sqlite and boost deps

--- .circleci/setup_debian.sh
+++ .circleci/setup_debian.sh
@@ -2,13 +2,19 @@
 
 echo "========> Install basic config"
 apt-get update
-apt-get install -y apt-transport-https wget curl git python build-essential libx11-xcb-dev sqlite3
+apt-get install -y apt-transport-https wget curl git python build-essential libx11-xcb-dev
 
 echo "========> Install C++ dependencies"
 apt-get install -y g++ make cmake
 
 echo "========> Install Qt5"
 apt-get install -y qt5-default libqt5websockets5 libqt5websockets5-dev
+
+echo "========> Install Sqlite"
+apt-get install -y sqlite3 sqlite libsqlite3-dev
+
+echo "========> Install Boost"
+apt-get install -y libboost-all-dev
 
 echo "========> Install node"
 curl -sL https://deb.nodesource.com/setup_9.x | bash -
