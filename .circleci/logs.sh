4ab95a37aa5f79ce93a826005a5bb2557b8568c4
KhalilBellakrid <khalil.bellakrid@ledger.fr>
2018-03-15 15:38:47 +0100

    add link_directories to sqlite3 and set compile option -fPIC in soci_sqlite3 and sqlite3

--- .circleci/logs.sh
+++ .circleci/logs.sh
@@ -7,3 +7,6 @@ ls -la core/lib/sqlite3
 
 echo "=====> lib-ledger-core-build/core/src/libledger-core.so"
 objdump -t core/src/libledger-core.so | grep sqlite3_open_
+
+echo "=====> lib-ledger-core-build/core/lib/sqlite3/libsqlite3.a"
+nm core/lib/sqlite3/libsqlite3.a | grep sqlite3_open_
