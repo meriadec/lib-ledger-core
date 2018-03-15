b6694709170485f5fd8afd63249b6c620da62018
KhalilBellakrid <khalil.bellakrid@ledger.fr>
2018-03-15 13:49:59 +0100

    add logs.sh and make sqlite3 PIC

---
+++ .circleci/logs.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+cd ../lib-ledger-core-build
+echo "=====> lib-ledger-core-build/core/lib/sqlite3"
+ls -la core/lib/sqlite3
+
+
+echo "=====> lib-ledger-core-build/core/src/libledger-core.so"
+objdump -t libledger-core.so | grep sqlite3_open_
